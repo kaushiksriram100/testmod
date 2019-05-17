289c12ee05f8b28371c028838286c720bfbc9d2d
Sriram Kaushik <skaushik@box.com>
2019-05-17 11:45:24 -0700

    d

--- testmod.go
+++ testmod.go
@@ -3,8 +3,8 @@ package testmod
 
 import "fmt"
 
-func Hi(name string, age int, school string) string {
-	return fmt.Sprintf("hello, %s and %d and %s", name, age, school)
+func Hi(name string, age int, school string, city string) string {
+	return fmt.Sprintf("hello, %s and %d and %s and %s", name, age, school, city)
 }
 
 
